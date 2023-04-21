2eab3a9ada266d5b82558de1c1d5b4b214f9fd5b
Eric Frey <ericlfrey@gmail.com>
2023-04-21 14:20:20 -0500

    Initial setup complete. Additionally added Post dsata

---
+++ models/post.py
@@ -0,0 +1,10 @@
+class Post():
+    """doc"""
+
+    def __init__(self, id, user_id, category_id, title, publication_date, content):
+        self.id = id
+        self.user_id = user_id
+        self.category_id = category_id
+        self.title = title
+        self.publication_date = publication_date
+        self.content = content
